8b7bb4aad94549d38f2969b9be928fc5f74ba93d
David Kviloria <datokviloria@gmail.com>
2020-10-11 13:46:52 +0400

    Initial Commit

---
+++ 2dc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2dc
+//
+//  Created by David Kviloria on 10/11/20.
+//  Copyright © 2020 David Kviloria. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
